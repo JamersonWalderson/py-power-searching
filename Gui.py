637203a0929d084be415217c4faf3575bc769429
jamesw.97 <jamersonwalderson@gmail.com>
2020-01-22 13:46:06 -0300

    update: versõa funcional
    Versão funcional (quase um beta) já integrada ao PyQt5

--- Gui.py
+++ Gui.py
@@ -1,20 +1,64 @@
-import sys
+# https://nitratine.net/blog/post/how-to-import-a-pyqt5-ui-file-in-a-python-gui/
+# https://tiforadacaixa.blogspot.com/2019/09/pyside2-pyqt-estruturando-projeto-em-mvc.html
+
 from PyQt5 import QtWidgets, uic
-from search import Search
+from modelProcurar import *
+from threading import Thread
+import sys
+
+class Ui(QtWidgets.QMainWindow):
+    
+    def __init__(self):
+        super(Ui, self).__init__()
+        uic.loadUi('main-designer.ui', self)
+
+        # findchield() = serve para encontrar o modulo do item
+        self.btProcurar = self.findChild(QtWidgets.QPushButton, 'btProcurar')
+        self.btProcurar.clicked.connect(self.realizar_pesquisa)
+
+        self.btLimpar = self.findChild(QtWidgets.QPushButton, 'btLimpar')
+        self.btLimpar.clicked.connect(self.limpar_tela)
+
+        self.btBaixar = self.findChild(QtWidgets.QPushButton, 'btBaixar')
+        self.btBaixar.clicked.connect(self.baixar_item_selecionado)
+
+        self.btbaixar_tudo = self.findChild(QtWidgets.QPushButton, 'btBaixarTudo')
+        self.btbaixar_tudo.clicked.connect(self.baixar_tudo)
+
+        # Armazena o valor digitado no QLineEdit na variavel assunto_da_pesquisa
+        self.assunto_da_pesquisa = self.findChild(QtWidgets.QLineEdit, 'txtPesquisar')
+        
+
+        self.show()
+
+    def tema_pesquisa(self, assunto_pesquisado):
+        assunto_pesquisado = assunto_pesquisado
+        return assunto_pesquisado
+
+    def realizar_pesquisa(self):
+        encontrados = []
+        assunto_pesquisado = self.txtPesquisar.text()
+        procurar = ModelProcurar(assunto_pesquisado,20)
+        encontrados.extend(procurar.buscar())
+        self.listVisualizar.addItems(encontrados)
+        return encontrados
+
+    def limpar_tela(self):
+        self.listVisualizar.clear()
+    
+    def baixar_item_selecionado(self):
+        print ('#baixar')
+        # codigo
+    
+    def baixar_tudo(self):
+        # a variável assunto recebe o valor digitado no QlineEdit
+        assunto = self.assunto_da_pesquisa.text()
+        baixar_lista = ModelProcurar(assunto, 20)
+        baixar_lista.baixarTudo()
 
 
-janela = QtWidgets.QApplication(sys.argv)
-interface = uic.loadUi('main-designer.ui')
-# procurar = Search('digimon',5)
-
-def listar():
-    pesquisa = interface.txtPesquisar.text()
-    procurar = Search(pesquisa,5)   
-    #print (pesquisa)
-    interface.listVisualizar.addItems(procurar.buscar())
-
-interface.btProcurar.clicked.connect(listar)
 
 
-interface.show()
-sys.exit(janela.exec())
+app = QtWidgets.QApplication(sys.argv)
+window = Ui()
+app.exec_()
--- modelProcurar.py
+++ modelProcurar.py
@@ -1,31 +1,47 @@
+# search.py
+# -*- coding: utf-8 -*-
 '''
     jamersonwalderson@gmail.com
     Conteúdo usado para suporte
     http://pythonclub.com.br/introducao-classes-metodos-python-basico.html
 '''
 from googlesearch import search
+from threading import Thread
+import platform
 import wget
 
-class Search:
+
+class ModelProcurar:
     
     def __init__(self, procurar, qt_itens):
         self.procurar = procurar + 'filetype:pdf'
         self.qt_itens = qt_itens
-
-
-       
 
     def buscar(self):
         # procurar = input('O que deseja buscar? ') + ' filetype:pdf'
         # qt_itens = int (input('Quantidade de resultados: '))
         # num =   int(input("Quantantidadede resultados a ser buscado: "))
         list_url = [] 
-
         for urls in search(self.procurar, num=1, start=0, stop=self.qt_itens, pause=2.0):
-            list_url.append(urls)
-        
+            list_url.append(urls)       
         return list_url
     
+    
+    def baixarTudo(self):
+        baixados_com_sucesso = 0      
+        for urls in search(self.procurar, num=1, start=0, stop=self.qt_itens, pause=2.0):
+            try:
+                baixados_com_sucesso+=1
+                print ( baixados_com_sucesso )
+                wget.download(urls)
+            except:
+                print ('O download de ', urls, 'falhou.')
+    
+    def baixarSelecionado(self, download_item):
+        wget.download(download_item)
+
+    def testando(self):
+        print ('funfo')
 
 '''
 teste = Search('digimon',5)
